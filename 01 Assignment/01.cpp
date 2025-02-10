dc5231ab2517b2764bcdd6504315202f26426624
md8-habibullah <md8.habibullah@gmail.com>
2025-02-10 19:01:19 +0600

    1st solver self

--- 01 Assignment/01.cpp
+++ 01 Assignment/01.cpp
@@ -1,1 +1,24 @@
-#
+#include <iostream>
+using namespace std;
+
+void shiftLeft (int arr[9], int n){
+    for (int i = n-1;i < 7; i++){
+        arr[i-n] = arr[i];
+    };
+    for (int j = 7 -n -1; j < 7; j++){
+        arr[j] =0;
+    }
+    
+    for(int i = 0; i < 7; i++){
+        cout << arr[i] <<" ";
+    };
+};
+
+int main()
+{
+    int k = 3;
+    // cin >> k;
+    int source[7] = {245,536,47,5636,969,573,453};
+    shiftLeft(source,k);
+    return 0;
+}
